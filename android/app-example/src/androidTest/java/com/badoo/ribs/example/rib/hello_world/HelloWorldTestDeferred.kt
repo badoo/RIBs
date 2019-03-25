971eb1673fbf98d6a50982030d1a95f3e7b8f847
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2019-03-25 15:37:59 +0000

    Fix comilation errors, run android tests on travis

--- android/app-example/src/androidTest/java/com/badoo/ribs/example/rib/hello_world/HelloWorldTestDeferred.kt
+++ android/app-example/src/androidTest/java/com/badoo/ribs/example/rib/hello_world/HelloWorldTestDeferred.kt
@@ -1,5 +1,6 @@
 package com.badoo.ribs.example.rib.hello_world
 
+import android.content.Intent
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -7,10 +8,14 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import com.badoo.common.ribs.RibsRule
 import com.badoo.ribs.RibTestActivity
 import com.badoo.ribs.android.ActivityStarter
+import com.badoo.ribs.android.IntentCreator
+import com.badoo.ribs.android.PermissionRequester
+import com.badoo.ribs.core.Identifiable
 import com.badoo.ribs.core.directory.Directory
 import com.badoo.ribs.example.R
 import com.badoo.ribs.example.app.AppRibCustomisations
 import com.badoo.ribs.example.rib.hello_world.builder.HelloWorldBuilder
+import io.reactivex.Observable
 import io.reactivex.Observable.empty
 import io.reactivex.ObservableSource
 import io.reactivex.functions.Consumer
@@ -27,7 +32,17 @@ class HelloWorldTestDeferred {
             override fun helloWorldInput(): ObservableSource<HelloWorld.Input> = empty()
             override fun helloWorldOutput(): Consumer<HelloWorld.Output> = Consumer {}
             override fun ribCustomisation(): Directory = AppRibCustomisations
-            override fun activityStarter(): ActivityStarter = ribTestActivity
+            override fun activityStarter(): ActivityStarter = object : ActivityStarter {
+                override fun startActivity(createIntent: IntentCreator.() -> Intent) = TODO()
+                override fun startActivityForResult(client: Identifiable, requestCode: Int, createIntent: IntentCreator.() -> Intent) = TODO()
+                override fun events(client: Identifiable): Observable<ActivityStarter.ActivityResultEvent> = Observable.empty()
+            }
+
+            override fun permissionRequester(): PermissionRequester = object : PermissionRequester {
+                override fun checkPermissions(client: Identifiable, permissions: Array<String>) = TODO()
+                override fun requestPermissions(client: Identifiable, requestCode: Int, permissions: Array<String>) = TODO()
+                override fun events(client: Identifiable): Observable<PermissionRequester.RequestPermissionsEvent> = Observable.empty()
+            }
         }).build()
 
     @Test

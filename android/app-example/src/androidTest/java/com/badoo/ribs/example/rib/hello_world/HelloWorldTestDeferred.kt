8184c6b9dc591bb78568bd47daf066fd21942328
Andrei Shikov <andreiishikov@gmail.com>
2019-04-01 18:27:52 +0100

    Fix example base

--- android/app-example/src/androidTest/java/com/badoo/ribs/example/rib/hello_world/HelloWorldTestDeferred.kt
+++ android/app-example/src/androidTest/java/com/badoo/ribs/example/rib/hello_world/HelloWorldTestDeferred.kt
@@ -1,5 +1,6 @@
 package com.badoo.ribs.example.rib.hello_world
 
+import android.content.Context
 import android.content.Intent
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -8,7 +9,6 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import com.badoo.common.ribs.RibsRule
 import com.badoo.ribs.RibTestActivity
 import com.badoo.ribs.android.ActivityStarter
-import com.badoo.ribs.android.IntentCreator
 import com.badoo.ribs.android.PermissionRequester
 import com.badoo.ribs.core.Identifiable
 import com.badoo.ribs.core.directory.Directory
@@ -33,8 +33,8 @@ class HelloWorldTestDeferred {
             override fun helloWorldOutput(): Consumer<HelloWorld.Output> = Consumer {}
             override fun ribCustomisation(): Directory = AppRibCustomisations
             override fun activityStarter(): ActivityStarter = object : ActivityStarter {
-                override fun startActivity(createIntent: IntentCreator.() -> Intent) = TODO()
-                override fun startActivityForResult(client: Identifiable, requestCode: Int, createIntent: IntentCreator.() -> Intent) = TODO()
+                override fun startActivity(createIntent: Context.() -> Intent) = TODO()
+                override fun startActivityForResult(client: Identifiable, requestCode: Int, createIntent: Context.() -> Intent) = TODO()
                 override fun events(client: Identifiable): Observable<ActivityStarter.ActivityResultEvent> = Observable.empty()
             }
 

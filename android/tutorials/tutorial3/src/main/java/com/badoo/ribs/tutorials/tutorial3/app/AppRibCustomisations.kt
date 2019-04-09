3ec2d2d40031aaf6e48a43dbf7f8e5f8a8b3f298
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-04-09 19:16:40 +0100

    Copy tutorial2 to tutorial3 starting point

---
+++ android/tutorials/tutorial3/src/main/java/com/badoo/ribs/tutorials/tutorial3/app/AppRibCustomisations.kt
@@ -0,0 +1,5 @@
+package com.badoo.ribs.tutorials.tutorial3.app
+
+import com.badoo.ribs.core.directory.ViewCustomisationDirectory
+
+object AppRibCustomisations : ViewCustomisationDirectory()
---
+++ android/tutorials/tutorial3/src/main/java/com/badoo/ribs/tutorials/tutorial3/rib/greetings_container/builder/GreetingsContainerModule.kt
@@ -0,0 +1,49 @@
+package com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.builder
+
+import com.badoo.ribs.core.Node
+import com.badoo.ribs.core.view.ViewFactory
+import com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.GreetingsContainer
+import com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.GreetingsContainerInteractor
+import com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.GreetingsContainerRouter
+import com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.GreetingsContainerView
+import dagger.Provides
+import io.reactivex.functions.Consumer
+
+@dagger.Module
+internal object GreetingsContainerModule {
+
+    @GreetingsContainerScope
+    @Provides
+    @JvmStatic
+    internal fun router(
+        // pass component to child rib builders, or remove if there are none
+        component: GreetingsContainerComponent
+    ): GreetingsContainerRouter =
+        GreetingsContainerRouter()
+
+    @GreetingsContainerScope
+    @Provides
+    @JvmStatic
+    internal fun interactor(
+        router: GreetingsContainerRouter,
+        output: Consumer<GreetingsContainer.Output>
+    ): GreetingsContainerInteractor =
+        GreetingsContainerInteractor(
+            router = router,
+            output = output
+        )
+
+    @GreetingsContainerScope
+    @Provides
+    @JvmStatic
+    internal fun node(
+        viewFactory: ViewFactory<GreetingsContainerView>,
+        router: GreetingsContainerRouter,
+        interactor: GreetingsContainerInteractor
+    ) : Node<GreetingsContainerView> = Node(
+        identifier = object : GreetingsContainer {},
+        viewFactory = viewFactory,
+        router = router,
+        interactor = interactor
+    )
+}

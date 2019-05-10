22da27f64c6c173d550a691d7463cb4a9fd94e6e
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-05-10 15:37:03 +0100

    Add example for scoped customisation

--- android/app-example/src/main/java/com/badoo/ribs/example/app/AppRibCustomisations.kt
+++ android/app-example/src/main/java/com/badoo/ribs/example/app/AppRibCustomisations.kt
@@ -1,5 +1,27 @@
 package com.badoo.ribs.example.app
 
+import com.badoo.ribs.core.directory.Directory
 import com.badoo.ribs.core.directory.ViewCustomisationDirectory
+import com.badoo.ribs.core.directory.inflateOnDemand
+import com.badoo.ribs.example.R
+import com.badoo.ribs.example.rib.dialog_example.DialogExample
+import com.badoo.ribs.example.rib.foo_bar.FooBar
+import com.badoo.ribs.example.rib.lorem_ipsum.LoremIpsum
+import com.badoo.ribs.example.rib.switcher.Switcher
 
-object AppRibCustomisations : ViewCustomisationDirectory()
+object AppRibCustomisations : Directory by customisations({
+    Switcher::class {
+        DialogExample::class {
+            + LoremIpsum.Customisation(
+                viewFactory = inflateOnDemand(R.layout.rib_lorem_ipsum_override)
+            )
+        }
+    }
+
+    + FooBar.Customisation(
+        viewFactory = inflateOnDemand(R.layout.rib_foobar_override)
+    )
+})
+
+fun customisations(block: ViewCustomisationDirectory.() -> Unit): ViewCustomisationDirectory =
+    ViewCustomisationDirectory().apply(block)

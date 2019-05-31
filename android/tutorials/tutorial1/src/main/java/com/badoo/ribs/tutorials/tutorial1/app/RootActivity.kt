9e94557a0669069b5ecee840e5debd0b098e3a0e
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-05-31 14:07:42 +0100

    Cleanup

--- android/tutorials/tutorial1/src/main/java/com/badoo/ribs/tutorials/tutorial1/app/RootActivity.kt
+++ android/tutorials/tutorial1/src/main/java/com/badoo/ribs/tutorials/tutorial1/app/RootActivity.kt
@@ -1,14 +1,10 @@
 package com.badoo.ribs.tutorials.tutorial1.app
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.view.ViewGroup
 import com.badoo.ribs.android.RibActivity
 import com.badoo.ribs.core.Node
 import com.badoo.ribs.tutorials.tutorial1.R
-import com.badoo.ribs.tutorials.tutorial1.rib.hello_world.HelloWorld
-import com.badoo.ribs.tutorials.tutorial1.rib.hello_world.builder.HelloWorldBuilder
-import io.reactivex.functions.Consumer
 
 /** The tutorial app's single activity */
 class RootActivity : RibActivity() {
@@ -22,11 +18,5 @@ class RootActivity : RibActivity() {
         get() = findViewById(R.id.root)
 
     override fun createRib(): Node<*> =
-        HelloWorldBuilder(
-            object : HelloWorld.Dependency {
-                override fun helloWorldOutput(): Consumer<HelloWorld.Output> = Consumer {
-                    Snackbar.make(rootViewGroup, "Hello world!", Snackbar.LENGTH_SHORT).show()
-                }
-            }
-        ).build()
+        TODO("Create HelloWorldBuilder, supply dependencies in constructor, return built Node")
 }
--- android/tutorials/tutorial4/src/main/java/com/badoo/ribs/tutorials/tutorial4/app/AppRibCustomisations.kt
+++
@@ -1,5 +0,0 @@
-package com.badoo.ribs.tutorials.tutorial4.app
-
-import com.badoo.ribs.core.directory.ViewCustomisationDirectory
-
-object AppRibCustomisations : ViewCustomisationDirectory()

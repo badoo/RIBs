1fa4e9b07529cee9d1dde1a5c67ece35b661d085
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-06-07 13:21:54 +0100

    Fix detekt

--- android/tutorials/tutorial3/src/main/java/com/badoo/ribs/tutorials/tutorial3/rib/greetings_container/GreetingsContainerRouter.kt
+++ android/tutorials/tutorial3/src/main/java/com/badoo/ribs/tutorials/tutorial3/rib/greetings_container/GreetingsContainerRouter.kt
@@ -2,7 +2,6 @@ package com.badoo.ribs.tutorials.tutorial3.rib.greetings_container
 
 import android.os.Parcelable
 import com.badoo.ribs.core.Router
-import com.badoo.ribs.core.routing.action.AttachRibRoutingAction
 import com.badoo.ribs.core.routing.action.AttachRibRoutingAction.Companion.attach
 import com.badoo.ribs.core.routing.action.RoutingAction
 import com.badoo.ribs.tutorials.tutorial3.rib.greetings_container.GreetingsContainerRouter.Configuration

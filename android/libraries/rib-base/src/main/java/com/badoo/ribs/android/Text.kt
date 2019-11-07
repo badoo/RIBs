fe7daee5ad961d0e978e1d55dcbc1c6edf5f7d7b
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-11-07 14:42:54 +0000

    Rename ResId to Resource

--- android/libraries/rib-base/src/main/java/com/badoo/ribs/android/Text.kt
+++ android/libraries/rib-base/src/main/java/com/badoo/ribs/android/Text.kt
@@ -4,7 +4,7 @@ import android.content.Context
 
 /**
  * An abstraction over text, so that you can provide it from different sources.
- * 
+ *
  * In case the default implementations are not good enough, feel free to implement your own.
  */
 interface Text {
@@ -16,7 +16,7 @@ interface Text {
             string
     }
 
-    class ResId(private val resId: Int, private vararg val formatArgs: Any) : Text {
+    class Resource(private val resId: Int, private vararg val formatArgs: Any) : Text {
         override fun resolve(context: Context): String =
             context.resources.getString(resId, formatArgs)
     }

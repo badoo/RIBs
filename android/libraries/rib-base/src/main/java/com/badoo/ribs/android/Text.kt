0d50c3301d2ca2c60fcf55f083acc671887d8696
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-11-07 14:37:15 +0000

    Add documentation

--- android/libraries/rib-base/src/main/java/com/badoo/ribs/android/Text.kt
+++ android/libraries/rib-base/src/main/java/com/badoo/ribs/android/Text.kt
@@ -2,6 +2,11 @@ package com.badoo.ribs.android
 
 import android.content.Context
 
+/**
+ * An abstraction over text, so that you can provide it from different sources.
+ * 
+ * In case the default implementations are not good enough, feel free to implement your own.
+ */
 interface Text {
 
     fun resolve(context: Context): String

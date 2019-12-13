cf48357c9c4387a653d9d3ec3cb69025e2e3baaa
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-12-13 14:14:39 +0000

    Fix tests

--- android/libraries/rib-base/src/test/java/com/badoo/ribs/core/helper/TestView.kt
+++ android/libraries/rib-base/src/test/java/com/badoo/ribs/core/helper/TestView.kt
@@ -2,6 +2,7 @@ package com.badoo.ribs.core.helper
 
 import android.view.ViewGroup
 import com.badoo.ribs.core.Rib
+import com.badoo.ribs.core.Node
 import com.badoo.ribs.core.view.RibView
 import com.nhaarman.mockitokotlin2.mock
 
@@ -9,6 +10,6 @@ class TestView : RibView {
 
     override val androidView: ViewGroup = mock()
 
-    override fun getParentViewForChild(child: Rib): ViewGroup? =
+    override fun getParentViewForChild(child: Node<*>) =
         androidView
 }

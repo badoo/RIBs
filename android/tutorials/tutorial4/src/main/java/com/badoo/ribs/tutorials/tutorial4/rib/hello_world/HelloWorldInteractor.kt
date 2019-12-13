0fcb4b23211e3e19b809e8129a2b880d3e6201b2
Andrei Shikov <andreiishikov@gmail.com>
2019-12-13 00:49:56 +0000

    Revert accidental interactor changes

--- android/tutorials/tutorial4/src/main/java/com/badoo/ribs/tutorials/tutorial4/rib/hello_world/HelloWorldInteractor.kt
+++ android/tutorials/tutorial4/src/main/java/com/badoo/ribs/tutorials/tutorial4/rib/hello_world/HelloWorldInteractor.kt
@@ -2,9 +2,19 @@ package com.badoo.ribs.tutorials.tutorial4.rib.hello_world
 
 import android.os.Bundle
 import androidx.lifecycle.Lifecycle
+import com.badoo.mvicore.android.lifecycle.startStop
+import com.badoo.mvicore.binder.using
+import com.badoo.ribs.android.Text
 import com.badoo.ribs.core.Interactor
+import com.badoo.ribs.tutorials.tutorial4.R
+import com.badoo.ribs.tutorials.tutorial4.rib.hello_world.mapper.ViewEventToOutput
+import com.badoo.ribs.tutorials.tutorial4.util.User
+import io.reactivex.functions.Consumer
 
 class HelloWorldInteractor(
+    user: User,
+    config: HelloWorld.Config,
+    private val output: Consumer<HelloWorld.Output>,
     savedInstanceState: Bundle?
 ) : Interactor<HelloWorldView>(
     savedInstanceState = savedInstanceState,
@@ -13,5 +23,15 @@ class HelloWorldInteractor(
 
     override fun onViewCreated(view: HelloWorldView, viewLifecycle: Lifecycle) {
         super.onViewCreated(view, viewLifecycle)
+        view.accept(initialViewModel)
+        viewLifecycle.startStop {
+            bind(view to output using ViewEventToOutput)
+        }
     }
+
+    private val initialViewModel =
+        HelloWorldView.ViewModel(
+            titleText = Text.Resource(R.string.hello_world_title, user.name()),
+            welcomeText = config.welcomeMessage
+        )
 }

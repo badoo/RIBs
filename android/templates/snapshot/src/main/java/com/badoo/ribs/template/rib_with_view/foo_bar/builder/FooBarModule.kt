1b2ad1262fea06b98eb1722adbff455b4d289cb8
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-04-09 16:38:13 +0200

    Remove unused imports

--- android/templates/snapshot/src/main/java/com/badoo/ribs/template/rib_with_view/foo_bar/builder/FooBarModule.kt
+++ android/templates/snapshot/src/main/java/com/badoo/ribs/template/rib_with_view/foo_bar/builder/FooBarModule.kt
@@ -3,15 +3,11 @@ package com.badoo.ribs.template.rib_with_view.foo_bar.builder
 
 import com.badoo.ribs.core.builder.BuildParams
 import com.badoo.ribs.template.rib_with_view.foo_bar.FooBar
-import com.badoo.ribs.template.rib_with_view.foo_bar.FooBar.Input
-import com.badoo.ribs.template.rib_with_view.foo_bar.FooBar.Output
 import com.badoo.ribs.template.rib_with_view.foo_bar.FooBarInteractor
 import com.badoo.ribs.template.rib_with_view.foo_bar.FooBarNode
 import com.badoo.ribs.template.rib_with_view.foo_bar.FooBarRouter
 import com.badoo.ribs.template.rib_with_view.foo_bar.feature.FooBarFeature
 import dagger.Provides
-import io.reactivex.ObservableSource
-import io.reactivex.functions.Consumer
 
 @dagger.Module
 internal object FooBarModule {

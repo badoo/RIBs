5060fca0c1e067a5c2621a6418131e6ad5bba819
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-05-23 23:48:24 +0200

    Extract methods

--- android/libraries/rib-base/src/main/java/com/badoo/ribs/core/routing/history/RoutingHistoryDiffer.kt
+++ android/libraries/rib-base/src/main/java/com/badoo/ribs/core/routing/history/RoutingHistoryDiffer.kt
@@ -7,6 +7,12 @@ import com.badoo.ribs.core.routing.configuration.ConfigurationCommand.Add
 import com.badoo.ribs.core.routing.configuration.ConfigurationCommand.Deactivate
 import com.badoo.ribs.core.routing.configuration.ConfigurationCommand.Remove
 import com.badoo.ribs.core.routing.configuration.feature.BackStackFeature
+import com.badoo.ribs.core.routing.history.RoutingHistoryDiffer.activate
+import com.badoo.ribs.core.routing.history.RoutingHistoryDiffer.deactivate
+import com.badoo.ribs.core.routing.history.RoutingHistoryDiffer.diff
+import com.badoo.ribs.core.routing.history.RoutingHistoryElement.Activation.ACTIVE
+import com.badoo.ribs.core.routing.history.RoutingHistoryElement.Activation.INACTIVE
+import com.badoo.ribs.core.routing.history.RoutingHistoryElement.Activation.SHRUNK
 
 
 internal object RoutingHistoryDiffer {
@@ -28,37 +34,16 @@ internal object RoutingHistoryDiffer {
                 val previousIds = previous.map { it.routing.identifier }
                 val currentIds = current.map { it.routing.identifier }
 
-                // TODO extract remove
                 previousIds.minus(currentIds).reversed().forEach { identifier ->
-                    val toRemove = previous.find { it.routing.identifier == identifier }!!
-                    if (toRemove.activation == RoutingHistoryElement.Activation.ACTIVE) {
-                        commands += Deactivate(toRemove.routing)
-                    }
-                    commands += Remove(toRemove.routing)
-                    toRemove.overlays.reversed().forEach { overlay ->
-                        commands += Deactivate(overlay)
-                        commands += Remove(overlay)
-                    }
+                    val toRemove = previous.find(identifier)!!
+                    commands += if (toRemove.activation == ACTIVE) toRemove.deactivate() else emptySet()
+                    commands += toRemove.remove()
                 }
 
-                // TODO extract add
                 currentIds.minus(previousIds).forEach { identifier ->
-                    val toAdd = current.find { it.routing.identifier == identifier }!!
-                    commands += Add(toAdd.routing)
-
-                    // FIXME better abstraction for transitions between various Activations
-                    if (toAdd.activation == RoutingHistoryElement.Activation.ACTIVE) {
-                        commands += Activate(toAdd.routing)
-
-                    }
-
-                    toAdd.overlays.forEach { overlay ->
-                        commands += Add(overlay)
-
-                        if (toAdd.activation == RoutingHistoryElement.Activation.ACTIVE) {
-                            commands += Activate(overlay)
-                        }
-                    }
+                    val toAdd = current.find(identifier)!!
+                    commands += toAdd.add()
+                    commands += if (toAdd.activation == ACTIVE) toAdd.activate() else emptySet()
                 }
 
                 // TODO extract modify
@@ -66,44 +51,95 @@ internal object RoutingHistoryDiffer {
                     val t0 = previous.find { it.routing.identifier == identifier }!!
                     val t1 = current.find { it.routing.identifier == identifier }!!
 
-                    if (t1.activation != t0.activation) {
-                        when (t1.activation) {
-                            RoutingHistoryElement.Activation.ACTIVE -> {
-                                commands += Activate(t1.routing)
-                                t1.overlays.forEach { overlay ->
-                                    commands += Activate(overlay)
-                                }
-                            }
-                            RoutingHistoryElement.Activation.INACTIVE -> {
-                                commands += Deactivate(t1.routing)
-                                t1.overlays.forEach { overlay ->
-                                    commands += Deactivate(overlay)
-                                }
-                            }
-                            RoutingHistoryElement.Activation.SHRUNK -> Unit // FIXME implement
-                        }
-                    }
-
-                    if (t1.routing.meta != t0.routing.meta) {
-                        // FIXME implement
-                    }
-
-                    if (t1.overlays != t0.overlays) {
-                        // TODO make note in public doc that overlay indices are meant to be stable
-                        t0.overlays.minus(t1.overlays).forEach { overlay ->
-                            commands += Deactivate(overlay)
-                            commands += Remove(overlay)
-                        }
-
-                        t1.overlays.minus(t0.overlays).forEach { overlay ->
-                            commands += Add(overlay)
-                            commands += Activate(overlay)
-                        }
-                    }
+                    commands += diff(t0, t1)
                 }
 
                 commands
             }
         }
+
+    private fun <C : Parcelable> RoutingHistory<C>.find(identifier: Routing.Identifier): RoutingHistoryElement<C>? =
+        find { it.routing.identifier == identifier }
+
+    private fun <C : Parcelable> RoutingHistoryElement<C>.remove(): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+        commands += Remove(routing)
+        commands += overlays.map { Remove(it) }
+        return commands
+    }
+
+    private fun <C : Parcelable> RoutingHistoryElement<C>.deactivate(): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+        commands += Deactivate(routing)
+        commands += overlays.map { Deactivate(it) }
+        return commands
+    }
+
+    private fun <C : Parcelable> RoutingHistoryElement<C>.add(): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+        commands += Add(routing)
+        commands += overlays.map { Add(it) }
+        return commands
+    }
+
+    private fun <C : Parcelable> RoutingHistoryElement<C>.activate(): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+        commands += Activate(routing)
+        commands += overlays.map { Activate(it) }
+        return commands
+    }
+
+    private fun <C : Parcelable> diff(t0: RoutingHistoryElement<C>, t1: RoutingHistoryElement<C>): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+
+        commands += diffActivationChange(t0, t1)
+        commands += diffMetaChange(t0, t1)
+        commands += diffOverlayChange(t0, t1)
+
+        return commands
+    }
+
+    private fun <C : Parcelable> diffActivationChange(t0: RoutingHistoryElement<C>, t1: RoutingHistoryElement<C>): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+
+        if (t1.activation != t0.activation) {
+            commands += when (t1.activation) {
+                ACTIVE -> t1.activate()
+                INACTIVE -> t1.deactivate()
+                SHRUNK -> emptySet()// FIXME implement
+            }
+        }
+
+        return commands
+    }
+
+    private fun <C : Parcelable> diffMetaChange(t0: RoutingHistoryElement<C>, t1: RoutingHistoryElement<C>): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+
+        if (t1.routing.meta != t0.routing.meta) {
+            // FIXME implement
+        }
+
+        return commands
+    }
+
+    private fun <C : Parcelable> diffOverlayChange(t0: RoutingHistoryElement<C>, t1: RoutingHistoryElement<C>): Set<ConfigurationCommand<C>> {
+        val commands = mutableSetOf<ConfigurationCommand<C>>()
+
+        if (t1.overlays != t0.overlays) {
+            // TODO make note in public doc that overlay indices are meant to be stable
+            t0.overlays.minus(t1.overlays).forEach { overlay ->
+                if (t0.activation == ACTIVE) commands += Deactivate(overlay)
+                commands += Remove(overlay)
+            }
+
+            t1.overlays.minus(t0.overlays).forEach { overlay ->
+                commands += Add(overlay)
+                if (t0.activation == ACTIVE) commands += Activate(overlay)
+            }
+        }
+
+        return commands
+    }
 }
 

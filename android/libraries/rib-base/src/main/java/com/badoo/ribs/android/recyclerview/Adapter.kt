162d0d231c909af1e5888f05516350a945640cde
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-05-29 13:10:00 +0200

    Remove unused import

--- android/libraries/rib-base/src/main/java/com/badoo/ribs/android/recyclerview/Adapter.kt
+++ android/libraries/rib-base/src/main/java/com/badoo/ribs/android/recyclerview/Adapter.kt
@@ -13,7 +13,6 @@ import com.badoo.ribs.core.Node
 import com.badoo.ribs.core.routing.RoutingSource
 import com.badoo.ribs.core.routing.activator.ChildActivator
 import com.badoo.ribs.core.routing.history.Routing
-import com.badoo.ribs.util.RIBs
 import com.badoo.ribs.util.RIBs.errorHandler
 import io.reactivex.functions.Consumer
 import java.lang.ref.WeakReference

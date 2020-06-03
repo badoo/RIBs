a9682a758cdf04a0f40738d4bf4da75e1bffc432
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-06-03 20:49:57 +0200

    Remove outdated comments

--- libraries/rib-base/src/main/java/com/badoo/ribs/core/routing/history/RoutingHistoryElement.kt
+++ libraries/rib-base/src/main/java/com/badoo/ribs/core/routing/history/RoutingHistoryElement.kt
@@ -7,14 +7,10 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class RoutingHistoryElement<C : Parcelable>(
     val routing: Routing<C>,
-    // differ will not make an assumption that last one is always active,
-    // instead allows it to be controlled via this flag
     internal val activation: Activation = INACTIVE, // FIXME no default for this
     internal val overlays: List<Routing<C>> = emptyList()
 ) : Parcelable {
 
-    // FIXME check if can be unified
-    //  with [com.badoo.ribs.core.routing.configuration.ActivationState]
     enum class Activation {
         /**
          * On-screen

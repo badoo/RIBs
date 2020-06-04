479e19c7d32ca3ce6c2757e928a64be0ca51f4db
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-06-04 11:27:30 +0200

    Remove unused class

--- libraries/rib-base/src/main/java/com/badoo/ribs/android/recyclerview/client/RecyclerViewRibResolver.kt
+++
@@ -1,8 +0,0 @@
-package com.badoo.ribs.android.recyclerview.client
-
-import com.badoo.ribs.core.routing.action.RoutingAction
-
-interface RecyclerViewRibResolver<T> {
-
-    fun resolve(element: T): RoutingAction
-}

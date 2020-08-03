754b4f9f9ee5f84cad984e0a19dd99da6d905561
Andrei Shikov <andreiishikov@gmail.com>
2020-08-03 17:15:50 +0100

    Remove unused imports

--- libraries/rib-base/src/main/java/com/badoo/ribs/core/state/Rx2.kt
+++ libraries/rib-base/src/main/java/com/badoo/ribs/core/state/Rx2.kt
@@ -1,7 +1,6 @@
 package com.badoo.ribs.core.state
 
 import io.reactivex.Observable
-import io.reactivex.ObservableSource
 
 fun <T> Source<T>.wrap(): Observable<T> =
     Observable.create { emitter ->

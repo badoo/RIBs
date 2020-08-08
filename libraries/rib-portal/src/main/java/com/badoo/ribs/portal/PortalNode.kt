9adc406b4fb02b4c996b57d751f1bd25150690b2
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-08-08 13:03:21 +0200

    Extract rx-based workflow functionality to separate module

--- libraries/rib-portal/src/main/java/com/badoo/ribs/portal/PortalNode.kt
+++ libraries/rib-portal/src/main/java/com/badoo/ribs/portal/PortalNode.kt
@@ -1,6 +1,5 @@
 package com.badoo.ribs.portal
 
-import com.badoo.ribs.core.Node
 import com.badoo.ribs.core.Rib
 import com.badoo.ribs.core.modality.AncestryInfo
 import com.badoo.ribs.core.modality.BuildParams
@@ -9,13 +8,14 @@ import com.badoo.ribs.portal.PortalRouter.Configuration
 import com.badoo.ribs.portal.PortalRouter.Configuration.Content
 import com.badoo.ribs.routing.source.backstack.BackStackFeature
 import com.badoo.ribs.routing.source.backstack.operation.push
+import com.badoo.ribs.workflows.rx.RxWorkflowNode
 import io.reactivex.Single
 
 internal class PortalNode(
     buildParams: BuildParams<*>,
     plugins: List<Plugin>,
     private val backStack: BackStackFeature<Configuration>
-) : Node<Nothing>(
+) : RxWorkflowNode<Nothing>(
     buildParams = buildParams,
     viewFactory = null,
     plugins = plugins
--- sandbox/src/main/java/com/badoo/ribs/sandbox/rib/hello_world/HelloWorldNode.kt
+++ sandbox/src/main/java/com/badoo/ribs/sandbox/rib/hello_world/HelloWorldNode.kt
@@ -3,12 +3,12 @@ package com.badoo.ribs.sandbox.rib.hello_world
 import android.util.Log
 import com.badoo.ribs.clienthelper.connector.Connectable
 import com.badoo.ribs.clienthelper.connector.NodeConnector
-import com.badoo.ribs.core.Node
 import com.badoo.ribs.core.modality.BuildParams
 import com.badoo.ribs.core.plugin.Plugin
 import com.badoo.ribs.core.view.RibView
 import com.badoo.ribs.sandbox.rib.hello_world.HelloWorld.Input
 import com.badoo.ribs.sandbox.rib.hello_world.HelloWorld.Output
+import com.badoo.ribs.workflows.rx.RxWorkflowNode
 import io.reactivex.Single
 
 class HelloWorldNode(
@@ -16,7 +16,7 @@ class HelloWorldNode(
     viewFactory: ((RibView) -> HelloWorldView?)?,
     plugins: List<Plugin> = emptyList(),
     connector: NodeConnector<Input, Output> = NodeConnector()
-) : Node<HelloWorldView>(
+) : RxWorkflowNode<HelloWorldView>(
     buildParams = buildParams,
     viewFactory = viewFactory,
     plugins = plugins

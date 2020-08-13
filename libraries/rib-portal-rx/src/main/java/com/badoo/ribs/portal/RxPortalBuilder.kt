a779631f5b6e09f70058b03de9227ad7f71e1aa0
Andrei Shikov <andreiishikov@gmail.com>
2020-08-13 17:01:11 +0100

    Extract portal builder logic

--- libraries/rib-portal-rx/src/main/java/com/badoo/ribs/portal/RxPortalBuilder.kt
+++ libraries/rib-portal-rx/src/main/java/com/badoo/ribs/portal/RxPortalBuilder.kt
@@ -1,26 +1,19 @@
 package com.badoo.ribs.portal
 
 import com.badoo.ribs.annotation.ExperimentalApi
-import com.badoo.ribs.builder.SimpleBuilder
 import com.badoo.ribs.core.modality.BuildParams
 
 @ExperimentalApi
 class RxPortalBuilder(
-    private val dependency: Portal.Dependency
-) : SimpleBuilder<RxPortal>() {
+    dependency: Portal.Dependency
+) : BasePortalBuilder<RxPortal>(dependency) {
 
-    override fun build(buildParams: BuildParams<Nothing?>): RxPortal {
-        val interactor = PortalInteractor(
-            buildParams = buildParams
-        )
-        val router = PortalRouter(
-            buildParams = buildParams,
-            routingSource = interactor,
-            defaultRoutingAction = dependency.defaultRoutingAction().invoke(interactor),
-            transitionHandler = dependency.transitionHandler()
-        )
-
-        return RxPortalNode(
+    override fun createNode(
+        buildParams: BuildParams<Nothing?>,
+        interactor: PortalInteractor,
+        router: PortalRouter
+    ): RxPortal =
+        RxPortalNode(
             buildParams = buildParams,
             plugins = listOf(
                 interactor,
@@ -28,5 +21,4 @@ class RxPortalBuilder(
             ),
             backStack = interactor.backStack
         )
-    }
 }

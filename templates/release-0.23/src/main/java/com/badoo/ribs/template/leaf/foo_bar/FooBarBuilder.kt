31eca025669848cd13b16243d8debdb9fa0dc11d
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2020-12-10 22:46:49 +0100

    Spawn 0.23 templates from 0.22

---
+++ templates/release-0.23/src/main/java/com/badoo/ribs/template/leaf/foo_bar/FooBarBuilder.kt
@@ -0,0 +1,41 @@
+package com.badoo.ribs.template.leaf.foo_bar
+
+import com.badoo.ribs.core.builder.BuildParams
+import com.badoo.ribs.core.builder.SimpleBuilder
+import com.badoo.ribs.template.leaf.foo_bar.feature.FooBarFeature
+
+class FooBarBuilder(
+    private val dependency: FooBar.Dependency
+) : SimpleBuilder<FooBar>() {
+
+    override fun build(buildParams: BuildParams<Nothing?>): FooBar {
+        val customisation = buildParams.getOrDefault(FooBar.Customisation())
+        val feature = feature()
+        val interactor = interactor(buildParams, feature)
+
+        return node(buildParams, customisation, interactor)
+    }
+
+    private fun feature() =
+        FooBarFeature()
+
+    private fun interactor(
+        buildParams: BuildParams<*>,
+        feature: FooBarFeature
+    ) =
+        FooBarInteractor(
+            buildParams = buildParams,
+            feature = feature
+        )
+
+    private fun node(
+        buildParams: BuildParams<Nothing?>,
+        customisation: FooBar.Customisation,
+        interactor: FooBarInteractor
+    ) =
+        FooBarNode(
+            buildParams = buildParams,
+            viewFactory = customisation.viewFactory(null),
+            plugins = listOf(interactor)
+        )
+}

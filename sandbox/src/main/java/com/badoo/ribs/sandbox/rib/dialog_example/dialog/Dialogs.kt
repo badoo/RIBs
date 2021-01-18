9f79b737d92b118920de9cf084355c3a6ae2d84f
Manuel Perera <mapm14@gmail.com>
2021-01-18 12:08:17 +0100

    Remove some current issues from Detekt baseline and add return type to variables in Dialogs class

--- sandbox/src/main/java/com/badoo/ribs/sandbox/rib/dialog_example/dialog/Dialogs.kt
+++ sandbox/src/main/java/com/badoo/ribs/sandbox/rib/dialog_example/dialog/Dialogs.kt
@@ -1,14 +1,15 @@
 package com.badoo.ribs.sandbox.rib.dialog_example.dialog
 
+import com.badoo.ribs.android.dialog.Dialog
 import com.badoo.ribs.sandbox.rib.dialog_example.routing.DialogExampleChildBuilders
 
 class Dialogs internal constructor(
     builders: DialogExampleChildBuilders
 ) {
-    val themeDialog = ThemeDialog()
-    val simpleDialog = SimpleDialog()
-    val lazyDialog = LazyDialog()
-    val ribDialog = RibDialog(
+    val themeDialog: Dialog<Dialog.Event> = ThemeDialog()
+    val simpleDialog: Dialog<Dialog.Event> = SimpleDialog()
+    val lazyDialog: Dialog<Dialog.Event> = LazyDialog()
+    val ribDialog: Dialog<Dialog.Event> = RibDialog(
         builders.loremIpsum
     )
 }

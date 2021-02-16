12a06107b9af3d74a2455f5cf82f94dce18e3b4b
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2021-02-16 22:03:15 +0100

    Remove dagger templates

--- templates/release-0.26/src/main/java/com/badoo/ribs/template/node_dagger_build_param/foo_bar/builder/FooBarScope.kt
+++
@@ -1,7 +0,0 @@
-package com.badoo.ribs.template.node_dagger_build_param.foo_bar.builder
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-internal annotation class FooBarScope

d49169b3b3f16a99dd85c268cf6c620be0ab46f8
Joana Estafanell <joana.estafanell@team.bumble.com>
2021-02-19 18:19:21 +0100

    Rebase branch to master

---
+++ samples/transition-animations/src/main/java/com/badoo/ribs/samples/transitionanimations/RootActivity.kt
@@ -0,0 +1,10 @@
+package com.badoo.ribs.samples.transitionanimations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

3070f52e98621b7c18415b5349323db2e4f35a37
Vlad Cipariu <vlad.cipariu@team.bumble.com>
2021-03-25 14:30:57 +0000

    adds RootActivity as the entry point and adds a default layout, no ribs attached yet

--- samples/communication-between-nodes/src/main/java/com/badoo/ribs/samples/comms_nodes/app/RootActivity.kt
+++ samples/communication-between-nodes/src/main/java/com/badoo/ribs/samples/comms_nodes/app/RootActivity.kt
@@ -1,4 +1,22 @@
 package com.badoo.ribs.samples.comms_nodes.app
 
-class RootActivity {
+import android.os.Bundle
+import android.view.ViewGroup
+import com.badoo.ribs.android.RibActivity
+import com.badoo.ribs.core.Rib
+import com.badoo.ribs.samples.comms_nodes.R
+
+class RootActivity : RibActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+
+    override val rootViewGroup: ViewGroup
+        get() = findViewById(R.id.root)
+
+    override fun createRib(savedInstanceState: Bundle?): Rib {
+        TODO("Not yet implemented")
+    }
 }

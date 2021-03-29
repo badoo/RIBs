e36803f5417ac76d22c54255beadee88e1dda13c
Vlad Cipariu <vlad.cipariu@team.bumble.com>
2021-03-29 12:55:27 +0100

    reverts to functional interface.

--- libraries/rib-base/src/main/java/com/badoo/ribs/core/view/ViewFactory.kt
+++ libraries/rib-base/src/main/java/com/badoo/ribs/core/view/ViewFactory.kt
@@ -2,7 +2,7 @@ package com.badoo.ribs.core.view
 
 import androidx.lifecycle.Lifecycle
 
-interface ViewFactory<View> : (ViewFactory.Context) -> View {
+fun interface ViewFactory<View> : (ViewFactory.Context) -> View {
     class Context(
         val parent: RibView,
         val lifecycle: Lifecycle,

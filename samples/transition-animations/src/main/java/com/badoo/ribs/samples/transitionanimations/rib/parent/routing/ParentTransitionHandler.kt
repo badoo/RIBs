9407d64b28f9314f0283e5de520d47b78dfaa2a0
Joana Estafanell <joana.estafanell@team.bumble.com>
2021-04-19 19:56:48 +0200

    Animation duration increased to 1000ms, removed crossfader condition and included parent footer label.

--- samples/transition-animations/src/main/java/com/badoo/ribs/samples/transitionanimations/rib/parent/routing/ParentTransitionHandler.kt
+++ samples/transition-animations/src/main/java/com/badoo/ribs/samples/transitionanimations/rib/parent/routing/ParentTransitionHandler.kt
@@ -11,7 +11,7 @@ import com.badoo.ribs.routing.transition.handler.TransitionHandler
 import com.badoo.ribs.samples.transitionanimations.R
 import com.badoo.ribs.samples.transitionanimations.rib.parent.routing.ParentRouter.Configuration
 
-class ParentTransitionHandler(duration: Long = 750) : TransitionHandler.Multiple<Configuration>(
+class ParentTransitionHandler(duration: Long = 1000) : TransitionHandler.Multiple<Configuration>(
     listOf(
         SharedElements(
             params = listOf(
@@ -24,20 +24,21 @@ class ParentTransitionHandler(duration: Long = 750) : TransitionHandler.Multiple
             )
         ),
         CrossFader(
-            duration = duration,
-            condition = { it.direction == TransitionDirection.EXIT }
+            duration = duration
         ),
         Slider(
             duration = duration,
             condition = {
                 it.isNotEnteringFirstChild()
                     && it.isNotExitingLastChild()
-
             }
         )
     )
 )
 
+// TODO Refactor addedOrRemoved when https://github.com/badoo/RIBs/issues/286 is implemented.
+//  Meanwhile, because we're using BackStack.push() to load Configurations, we can safely use
+//  addedOrRemoved property to detect when user is reversing the animation
 fun TransitionElement<out Configuration>.isNotEnteringFirstChild(): Boolean =
     !(configuration == Configuration.Child1
         && direction == TransitionDirection.ENTER

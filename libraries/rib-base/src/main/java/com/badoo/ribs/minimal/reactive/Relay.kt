59cf4ff5c0d8093660052a4b6c82110b313c192d
CherryPerry <ya.cherryperry@yandex.ru>
2021-06-08 14:38:56 +0100

    Use copy-on-write list in Relay

--- libraries/rib-base/src/main/java/com/badoo/ribs/minimal/reactive/Relay.kt
+++ libraries/rib-base/src/main/java/com/badoo/ribs/minimal/reactive/Relay.kt
@@ -4,35 +4,21 @@ import com.badoo.ribs.minimal.reactive.Cancellable.Companion.cancellableOf
 
 
 open class Relay<T> : Source<T>, Emitter<T> {
-    private val listeners: MutableList<(T) -> Unit> = mutableListOf()
-
-    // listenersGuard allows listeners removal inside forEach,
-    // which is not allowed by default because of ConcurrentModificationException
-    private var listenersGuard = false
-
-    // store listeners to remove and remove them later
-    private val removeQueue: MutableList<(T) -> Unit> = mutableListOf()
+    // keep list read-only to avoid modification during iteration
+    private var listeners: List<(T) -> Unit> = emptyList()
 
     fun accept(value: T) {
         emit(value)
     }
 
     override fun emit(value: T) {
-        listenersGuard = true
         listeners.forEach { it.invoke(value) }
-        listenersGuard = false
-        listeners.removeAll(removeQueue)
-        removeQueue.clear()
     }
 
     override fun observe(callback: (T) -> Unit): Cancellable {
-        listeners += callback
+        listeners = listeners + callback
         return cancellableOf {
-            if (listenersGuard) {
-                removeQueue.add(callback)
-            } else {
-                listeners -= callback
-            }
+            listeners = listeners - callback
         }
     }
 }

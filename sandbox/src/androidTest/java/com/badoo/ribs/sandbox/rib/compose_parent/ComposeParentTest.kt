53f947bb8bdb2b44ba08d13e0e0bd0d11dbbc046
CherryPerry <ya.cherryperry@yandex.ru>
2021-08-02 10:51:04 +0100

    Resolve merge conflicts

--- sandbox/src/androidTest/java/com/badoo/ribs/sandbox/rib/compose_parent/ComposeParentTest.kt
+++ sandbox/src/androidTest/java/com/badoo/ribs/sandbox/rib/compose_parent/ComposeParentTest.kt
@@ -1,16 +1,14 @@
-package com.badoo.ribs.sandbox.rib
+package com.badoo.ribs.sandbox.rib.compose_parent
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.onNodeWithText
 import com.badoo.ribs.core.modality.BuildContext
-import com.badoo.ribs.sandbox.rib.compose_parent.ComposeParent
-import com.badoo.ribs.sandbox.rib.compose_parent.ComposeParentBuilder
 import com.badoo.ribs.test.RibsRule
 import org.junit.Rule
 import org.junit.Test
 
-class BasicComposeTest {
+class ComposeParentTest {
 
     @get:Rule
     val rule = AndroidComposeTestRule(

98329e39ede2c3b1f3acc0a915c7e4d82705cf11
Lachlan McKee <lachlan.mckee@team.bumble.com>
2024-09-23 16:04:32 +0100

    Removed intellij plugin due to lack of support and build issues

--- plugins/src/main/kotlin/com/badoo/ribs/templategenerator/generate/Token.kt
+++
@@ -1,9 +0,0 @@
-package com.badoo.ribs.templategenerator.generate
-
-import java.io.Serializable
-
-class Token(
-    val id: String,
-    val name: String,
-    val sourceValue: String
-) : Serializable
--- tooling/rib-intellij-plugin/src/main/java/com/badoo/ribs/plugin/template/Token.kt
+++
@@ -1,7 +0,0 @@
-package com.badoo.ribs.plugin.template
-
-class Token(
-    val id: String,
-    val name: String,
-    val sourceValue: String
-)

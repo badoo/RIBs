fa92c99b65cdd3f4e964ddb0f00255b983e6351c
Lachlan McKee <lachlan.mckee@team.bumble.com>
2024-09-23 18:38:17 +0100

    Updated Kotlin & Detekt

--- libraries/rib-rx2/src/main/java/com/badoo/ribs/rx2/adapter/Rx2.kt
+++ libraries/rib-rx2/src/main/java/com/badoo/ribs/rx2/adapter/Rx2.kt
@@ -3,7 +3,7 @@ package com.badoo.ribs.rx2.adapter
 import com.badoo.ribs.minimal.reactive.Source
 import io.reactivex.Observable
 
-fun <T> Source<T>.rx2(): Observable<T> =
+fun <T: Any> Source<T>.rx2(): Observable<T> =
     Observable.create { emitter ->
         val cancellable = observe { emitter.onNext(it) }
         emitter.setCancellable { cancellable.cancel() }
